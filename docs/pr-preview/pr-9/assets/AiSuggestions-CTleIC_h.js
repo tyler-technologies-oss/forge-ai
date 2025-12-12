b92964ead0a88809d9b758f3b826b342f372fb6f
nickonometry <nickonometry@users.noreply.github.com>
2025-12-12 17:45:07 +0000

    Remove preview for PR 9 🛬

--- docs/pr-preview/pr-9/assets/AiSuggestions-CTleIC_h.js
+++
@@ -1,6 +0,0 @@
-import{j as t,M as r,T as e,C as m}from"./blocks-BORlquAz.js";import{useMDXComponents as i}from"./index-BVIjL5xL.js";import{C as p}from"./CustomArgTypes-Da9IOX6n.js";import{A as a,D as c}from"./AiSuggestions.stories-CmwtwRgo.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-tTf1YFhn.js";import"./ai-suggestions-DsIXhdAL.js";import"./custom-element-UsVr97OX.js";import"./property-D1sRqruW.js";import"./when-CI7b_ccM.js";import"./utils-BGBq21po.js";import"./tooltip-BIm1aYoH.js";import"./state-CoFQ-eiY.js";import"./query-BpjciUlk.js";import"./base-DVmwUFg0.js";import"./class-map-D6J53kEk.js";import"./overlay-CnMC4RNa.js";function n(o){const s={h2:"h2",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:a}),`
-`,t.jsx(e,{}),`
-`,t.jsx(s.p,{children:"The AI Suggestions component displays interactive suggestion chips that users can select to quickly input common queries or responses."}),`
-`,t.jsx(m,{of:c}),`
-`,t.jsx(s.h2,{id:"api",children:"API"}),`
-`,t.jsx(p,{})]})}function I(o={}){const{wrapper:s}={...i(),...o.components};return s?t.jsx(s,{...o,children:t.jsx(n,{...o})}):n(o)}export{I as default};
