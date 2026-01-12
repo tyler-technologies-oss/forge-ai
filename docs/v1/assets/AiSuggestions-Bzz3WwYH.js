8a670348e2c52c29d089726f6cf93ae6e0a04645
DRiFTy17 <nicholskb17@gmail.com>
2026-01-12 14:41:16 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge-ai@c1ea87823b5bdb79c07b310a0d6848134b60b0b3 🚀

---
+++ docs/v1/assets/AiSuggestions-Bzz3WwYH.js
@@ -0,0 +1,6 @@
+import{j as t,M as r,T as e,C as m}from"./blocks-ttxEZoQ9.js";import{useMDXComponents as i}from"./index-DwN4--VT.js";import{C as p}from"./CustomArgTypes-CZrwTyn7.js";import{A as a,D as c}from"./AiSuggestions.stories-Diaa1kDm.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DbYUv31D.js";import"./ai-suggestions-B-F59nub.js";import"./custom-element-UsVr97OX.js";import"./property-DJ_SmNVs.js";import"./when-CI7b_ccM.js";import"./utils-BGBq21po.js";import"./tooltip-B1PewwDs.js";import"./state-DKROCAgB.js";import"./query-BpjciUlk.js";import"./base-DVmwUFg0.js";import"./class-map-DHPkP9wc.js";import"./overlay-zFbY7LdT.js";function n(o){const s={h2:"h2",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:a}),`
+`,t.jsx(e,{}),`
+`,t.jsx(s.p,{children:"The AI Suggestions component displays interactive suggestion chips that users can select to quickly input common queries or responses."}),`
+`,t.jsx(m,{of:c}),`
+`,t.jsx(s.h2,{id:"api",children:"API"}),`
+`,t.jsx(p,{})]})}function I(o={}){const{wrapper:s}={...i(),...o.components};return s?t.jsx(s,{...o,children:t.jsx(n,{...o})}):n(o)}export{I as default};
