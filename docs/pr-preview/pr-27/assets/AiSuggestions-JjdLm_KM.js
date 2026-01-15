dfb5bc9aeb066467bc2f3c03e0185a5c5a5dc857
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2026-01-15 11:56:51 +0000

    Remove preview for PR 27 🛬

--- docs/pr-preview/pr-27/assets/AiSuggestions-JjdLm_KM.js
+++
@@ -1,6 +0,0 @@
-import{j as t,M as r,T as e,C as m}from"./blocks-BzENXzRv.js";import{useMDXComponents as i}from"./index-BcaJ9aXF.js";import{C as p}from"./CustomArgTypes-5An7gywN.js";import{A as a,D as c}from"./AiSuggestions.stories-B9Nq12wW.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DMbuo427.js";import"./ai-suggestions-ni791voM.js";import"./custom-element-UsVr97OX.js";import"./property-CtXI_GOA.js";import"./when-CI7b_ccM.js";import"./utils-BGBq21po.js";import"./tooltip-fB3wd5DJ.js";import"./state-p5EnGfoQ.js";import"./query-BpjciUlk.js";import"./base-DVmwUFg0.js";import"./class-map-Z_RZVYSV.js";import"./overlay-CkHWG2Gf.js";function n(o){const s={h2:"h2",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:a}),`
-`,t.jsx(e,{}),`
-`,t.jsx(s.p,{children:"The AI Suggestions component displays interactive suggestion chips that users can select to quickly input common queries or responses."}),`
-`,t.jsx(m,{of:c}),`
-`,t.jsx(s.h2,{id:"api",children:"API"}),`
-`,t.jsx(p,{})]})}function I(o={}){const{wrapper:s}={...i(),...o.components};return s?t.jsx(s,{...o,children:t.jsx(n,{...o})}):n(o)}export{I as default};
