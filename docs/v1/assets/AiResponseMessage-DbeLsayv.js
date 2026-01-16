98b1028c3451591844d8ef53ccc340685f84a32d
DRiFTy17 <nicholskb17@gmail.com>
2026-01-16 14:38:55 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge-ai@5c0a9a0f40b3c15ff0d83ac585dbf8b0b30a19a2 🚀

--- docs/v1/assets/AiResponseMessage-DbeLsayv.js
+++
@@ -1,6 +0,0 @@
-import{j as n,M as r,T as i,C as a}from"./blocks-z4iZy5jk.js";import{useMDXComponents as e}from"./index-BNLrn6tD.js";import{C as m}from"./CustomArgTypes-DjMrJgB9.js";import{A as p,D as c}from"./AiResponseMessage.stories-jxYE7VYw.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DVvegsSV.js";import"./ai-response-message-D0oKuAb4.js";import"./custom-element-UsVr97OX.js";function t(s){const o={h2:"h2",p:"p",...e(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:p}),`
-`,n.jsx(i,{}),`
-`,n.jsx(o.p,{children:"The AI Response Message component displays AI-generated responses with consistent styling and optional action toolbars for user interaction."}),`
-`,n.jsx(a,{of:c}),`
-`,n.jsx(o.h2,{id:"api",children:"API"}),`
-`,n.jsx(m,{})]})}function g(s={}){const{wrapper:o}={...e(),...s.components};return o?n.jsx(o,{...s,children:n.jsx(t,{...s})}):t(s)}export{g as default};
