477eb9a731bf5869f41369f47eaa3bfd730e2926
Nichols, Kieran <kieran.nichols@tylertech.com>
2026-01-27 15:45:36 -0500

    chore: release 0.8.1

--- packages/ai-react/dist/ForgeAiResponseMessageToolbar.d.ts
+++ packages/ai-react/dist/ForgeAiResponseMessageToolbar.d.ts
@@ -24,6 +24,9 @@ export interface ForgeAiResponseMessageToolbarProps extends Pick<
   /** undefined */
   feedbackType?: ForgeAiResponseMessageToolbarElement["feedbackType"];
 
+  /** undefined */
+  feedbackReason?: ForgeAiResponseMessageToolbarElement["feedbackReason"];
+
   /** A space-separated list of the classes of the element. Classes allows CSS and JavaScript to select and access specific elements via the class selectors or functions like the method `Document.getElementsByClassName()`. */
   className?: string;
 
