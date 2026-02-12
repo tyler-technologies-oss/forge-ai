0b7ef4228317c4ae375004dd899f469eaa09c427
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2026-02-12 19:05:15 +0000

    Deploy preview for PR 38 🛫

---
+++ docs/pr-preview/pr-38/assets/AiButton-Cj5sGcTE.js
@@ -0,0 +1,6 @@
+import{j as t,M as s,T as r,C as m}from"./blocks-rtXGIGX_.js";import{useMDXComponents as i}from"./index-z8gOdIeX.js";import{C as a}from"./CustomArgTypes-BDf4swBS.js";import{A as p,D as c}from"./AiButton.stories-BsepjHdx.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CjyLkcAf.js";import"./ai-button-BRlRHLuX.js";import"./custom-element-UsVr97OX.js";import"./property-CCXGwIaz.js";import"./utils-BGBq21po.js";import"./ai-icon-BIBFBXw1.js";import"./when-CI7b_ccM.js";import"./ai-gradient-container-B0VYt1uL.js";function e(n){const o={h2:"h2",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(r,{}),`
+`,t.jsx(o.p,{children:"The AI Button is a specialized button component built on top of Tyler Forge's button component, designed specifically for AI-themed interfaces. It provides enhanced styling with a signature shimmer animation and consistent integration with AI chat widgets."}),`
+`,t.jsx(m,{of:c}),`
+`,t.jsx(o.h2,{id:"api",children:"API"}),`
+`,t.jsx(a,{})]})}function T(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{T as default};
---
+++ docs/pr-preview/pr-38/assets/AiVoiceInput-D1F5cGeD.js
@@ -0,0 +1,6 @@
+import{j as o,M as e,T as s,C as m}from"./blocks-rtXGIGX_.js";import{useMDXComponents as r}from"./index-z8gOdIeX.js";import{C as p}from"./CustomArgTypes-BDf4swBS.js";import{A as c,D as a}from"./AiVoiceInput.stories-DmxfWTo3.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CjyLkcAf.js";import"./ai-voice-input-BoGAExV_.js";import"./custom-element-UsVr97OX.js";import"./base-CwBp4TWH.js";import"./property-CCXGwIaz.js";import"./class-map-DwLEfOJ3.js";import"./tooltip-0qQm8HIB.js";import"./overlay-Cd7mO8-r.js";import"./when-CI7b_ccM.js";import"./index-BVqXfHSh.js";import"./service-adapter-CgOYZRmu.js";import"./constants-Co83GciG.js";function n(t){const i={code:"code",h2:"h2",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:c}),`
+`,o.jsx(s,{}),`
+`,o.jsxs(i.p,{children:["The ",o.jsx(i.code,{children:"forge-ai-voice-input"})," component provides voice input functionality for AI interactions."]}),`
+`,o.jsx(m,{of:a}),`
+`,o.jsx(i.h2,{id:"api",children:"API"}),`
+`,o.jsx(p,{})]})}function w(t={}){const{wrapper:i}={...r(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(n,{...t})}):n(t)}export{w as default};
