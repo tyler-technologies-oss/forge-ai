f6f296091374015dfa26273eaddb5b2c137ad8c0
Nichols, Kieran <kieran.nichols@tylertech.com>
2026-07-06 15:52:22 -0400

    feat(data-table): add CSV export button

--- packages/ai/src/lib/utils/index.ts
+++ packages/ai/src/lib/utils/index.ts
@@ -1,3 +1,4 @@
 export { DeleteThreadController } from './delete-thread-controller';
 export { InfiniteScrollController } from './infinite-scroll-controller';
 export { KeyboardNavigationController } from './keyboard-navigation-controller';
+export { buildCSVContent, downloadCSV, escapeCSVCell, sanitizeFilename } from './csv-utils';
