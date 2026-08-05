1e8f702f240c40d50911668b87c377b9e6cab253
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2026-08-05 19:52:01 +0000

    Remove preview for PR 96 🛬

--- docs/pr-preview/pr-96/assets/AiSuggestions-C62bGGQ_.js
+++
@@ -1,6 +0,0 @@
-import{j as t,M as i,T as r,C as m}from"./blocks-BtztPFNE.js";import{useMDXComponents as e}from"./index-BBue8hiB.js";import{C as p}from"./CustomArgTypes-BZrTVAV_.js";import{A as a,D as c}from"./AiSuggestions.stories-B4k4Q8rG.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CmlZJgPw.js";import"./utils-_nJVQAgP.js";import"./ai-suggestions-CQPfYeVz.js";import"./custom-element-UsVr97OX.js";import"./property-6B-lD8AL.js";import"./when-CI7b_ccM.js";import"./utils-BGBq21po.js";import"./tooltip-CLkTDClh.js";import"./class-map-Du48Bieu.js";import"./overlay-2RFBj5zj.js";import"./floating-ui.dom-alButkyJ.js";function n(o){const s={h2:"h2",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a}),`
-`,t.jsx(r,{}),`
-`,t.jsx(s.p,{children:"The AI Suggestions component displays interactive suggestion chips that users can select to quickly input common queries or responses."}),`
-`,t.jsx(m,{of:c}),`
-`,t.jsx(s.h2,{id:"api",children:"API"}),`
-`,t.jsx(p,{})]})}function v(o={}){const{wrapper:s}={...e(),...o.components};return s?t.jsx(s,{...o,children:t.jsx(n,{...o})}):n(o)}export{v as default};
--- docs/pr-preview/pr-96/assets/index-GPPQkZhP.js
+++
@@ -1,1 +0,0 @@
-import{f as n,g as o}from"./scaffold-D1pC1xrx.js";function e(){n(o)}export{e as d};
